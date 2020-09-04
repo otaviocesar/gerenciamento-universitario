320996be8fd9a263bda2382a8edc41d5161a1a26
Otavio Cesar Costa Santos <o.caesar@hotmail.com>
2020-09-04 19:50:26 -0300

    Adicionando documentação Open API e refatorando controllers

--- backend/src/main/java/br/com/universidade/gerenciamento/controller/AlunoController.java
+++ backend/src/main/java/br/com/universidade/gerenciamento/controller/AlunoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +15,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.universidade.gerenciamento.model.Aluno;
 import br.com.universidade.gerenciamento.repository.AlunoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@RequestMapping("/alunos")
+@Tag(name = "Alunos", description = "Grupo de endpoints para criar, listar, atualizar e deletar alunos")
 public class AlunoController {
 	
 	@Autowired
 	private AlunoRepository alunoRepository;
 	
-	@GetMapping(value = "/aluno")
+	@Operation(
+		summary = "Listar todos os alunos",
+		description = "Retorna uma lista com todas os alunos cadastrados"
+	)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Aluno> findAll() {
 		return alunoRepository.findAll();
 	}
 	
-	@GetMapping(value = "/aluno/{id}")
+	@Operation(summary = "Buscar aluno", description = "Buscar um aluno")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Aluno> findById(@PathVariable(value = "id") Long id) {
 		Optional<Aluno> aluno = alunoRepository.findById(id);
 		if (aluno.isPresent())
@@ -39,12 +50,22 @@ public class AlunoController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	@PostMapping(value = "/aluno")
+	@Operation(
+		summary = "Adicionar um aluno",
+		description = "Essa operacao salva um novo registro com as informacoes do aluno"
+	)
+	@PostMapping(
+		consumes = MediaType.APPLICATION_JSON_VALUE
+	)
 	public Aluno save(@Valid @RequestBody Aluno aluno) {
 		return alunoRepository.save(aluno);
 	}
 	
-	@PutMapping(value = "/aluno/{id}")
+	@Operation(
+		summary = "Atualizar aluno",
+		description = "Essa operacao atualiza os dados de um aluno"
+	)
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<Aluno> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Aluno newAluno) {
 		Optional<Aluno> oldAluno = alunoRepository.findById(id);
 		if (oldAluno.isPresent()) {
@@ -63,7 +84,11 @@ public class AlunoController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	@DeleteMapping(value = "/aluno/{id}")
+	@Operation(
+		summary = "Excluir aluno",
+		description = "Exclui o registro do aluno cadastrado"
+	)
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Object> delete(@PathVariable(value = "id") Long id) {
 		Optional<Aluno> aluno = alunoRepository.findById(id);
 		if (aluno.isPresent()) {
--- backend/src/main/java/br/com/universidade/gerenciamento/controller/CursoController.java
+++ backend/src/main/java/br/com/universidade/gerenciamento/controller/CursoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,23 +16,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.universidade.gerenciamento.model.Curso;
 import br.com.universidade.gerenciamento.repository.CursoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@RequestMapping("/cursos")
+@Tag(name = "Cursos", description = "Grupo de endpoints para criar, listar, atualizar e deletar cursos")
 public class CursoController {
 
 	@Autowired
 	private CursoRepository cursoRepository;
 
-	@GetMapping(value = "/curso")
+	@Operation(
+		summary = "Listar todos os cursos",
+		description = "Retorna uma lista com todas os cursos cadastrados"
+	)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Curso> findAll() {
 		return cursoRepository.findAll();
 	}
 	
-	@GetMapping(value = "/curso/{id}")
+	@Operation(summary = "Buscar curso", description = "Buscar um curso")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Curso> findById(@PathVariable(value = "id") Long id) {
 		Optional<Curso> curso = cursoRepository.findById(id);
 		if (curso.isPresent())
@@ -40,12 +51,22 @@ public class CursoController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	@PostMapping(value = "/curso")
+	@Operation(
+		summary = "Adicionar um curso",
+		description = "Essa operacao salva um novo registro com as informacoes do curso"
+	)
+	@PostMapping(
+		consumes = MediaType.APPLICATION_JSON_VALUE
+	)
 	public Curso save(@Valid @RequestBody Curso curso) {
 		return cursoRepository.save(curso);
 	}
 	
-	@PutMapping(value = "/curso/{id}")
+	@Operation(
+		summary = "Atualizar curso",
+		description = "Essa operacao atualiza os dados de um curso"
+	)
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<Curso> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Curso newCurso) {
 		Optional<Curso> oldCurso = cursoRepository.findById(id);
 		if (oldCurso.isPresent()) {
@@ -60,7 +81,11 @@ public class CursoController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	@DeleteMapping(value = "/curso/{id}")
+	@Operation(
+		summary = "Excluir curso",
+		description = "Exclui o registro do curso cadastrado"
+	)
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Object> delete(@PathVariable(value = "id") Long id) {
 		Optional<Curso> curso = cursoRepository.findById(id);
 		if (curso.isPresent()) {
